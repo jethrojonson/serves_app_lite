37f764c43ac5e135dbbc07653d863982ff2dbebb
jethrojonson <biojpg@gmail.com>
2023-02-18 15:20:35 +0100

    minor stuff added

---
+++ serves-app-lite-backend/src/main/java/com/salesianos/triana/dam/servesapplitebackend/ServeSLiteApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.dam.servesapplitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeSLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeSLiteApplication.class, args);
+	}
+
+}
---
+++ serves-app-lite-backend/src/test/java/com/salesianos/triana/dam/servesapplitebackend/ServeSLiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.dam.servesapplitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeSLiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
